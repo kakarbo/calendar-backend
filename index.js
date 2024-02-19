c2f8b5a74b233678a86f29e9a8c8ff5ffc4a6c52
Cristhian Carbonell <ccarbone@bancolombia.com.co>
2024-02-19 17:14:02 -0500

    Configurando Express

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hola Mundo!!!")
+const express = require('express')
+
+
+// Crear el servidor de express
+const app = express()
+
+// Rutas
+app.get('/', (req, res) => {
+
+    res.json({
+        ok: true
+    })
+})
+
+// Escuchar peticiones
+app.listen( 4000, () => {
+    console.log(`Server running in port ${ 4000 }`)
+})
