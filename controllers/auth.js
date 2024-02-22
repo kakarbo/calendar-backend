11c53360866d9dfd026cf2168b293c5093072c04
Cristhian Carbonell <ccarbone@bancolombia.com.co>
2024-02-22 17:20:36 -0500

    Express validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,18 @@
 const { response } = require('express')
+const { validationResult } = require('express-validator')
 
 const crearUsuario = ( req, res = response ) => {
 
     const { name, email, password } = req.body
 
-    if ( name.length < 5 ) {
+    // Manejo de errores
+
+    const errors = validationResult( req )
+
+    if ( !errors.isEmpty() ) {
         return res.status(400).json({
             ok: false,
-            msg: "El nombre debe de ser de 5 letras"
+            errors: errors.mapped()
         })
     }
 
@@ -23,6 +28,15 @@ const crearUsuario = ( req, res = response ) => {
 const loginUsuario = (req, res = response) => {
 
     const { email, password } = req.body
+
+    const errors = validationResult( req )
+
+    if ( !errors.isEmpty() ) {
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        })
+    }
 
     res.json({
         ok: true,
