b9613dba49a12e76ee3767f923dc4569979bb9f9
kakarbo <alexcarbo8809@gmail.com>
2024-02-22 17:26:49 -0500

    creación de custom middlewares

--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,15 @@ const { check } = require('express-validator')
 const router = Router()
 
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth')
+const { validarCampos } = require('../middlewares/validar-campos')
 
 router.post(
     '/new',
     [
         check('name', 'El nombre es obligatorio').not().isEmpty(),
         check('email', 'El email es obligatorio').isEmail(),
-        check('password', 'El password debe ser de 6 caracteres').isLength({ min: 6})
+        check('password', 'El password debe ser de 6 caracteres').isLength({ min: 6}),
+        validarCampos
     ],
     crearUsuario
 )
@@ -18,7 +20,8 @@ router.post(
     '/',
     [
         check('email', 'El email es obligatorio').isEmail(),
-        check('password', 'El password debe ser de 6 caracteres').isLength({ min: 6})
+        check('password', 'El password debe ser de 6 caracteres').isLength({ min: 6}),
+        validarCampos
     ],
     loginUsuario 
 )
