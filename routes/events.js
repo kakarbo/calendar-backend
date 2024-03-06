575d21bbdfeb2a13a119486bbb2a501d947307cc
kakarbo <alexcarbo8809@gmail.com>
2024-03-06 17:59:57 -0500

    validar campos necesarios

--- routes/events.js
+++ routes/events.js
@@ -3,8 +3,11 @@
     /api/events
 */
 const { Router } = require("express")
+const { check } = require("express-validator")
 const router = Router()
 
+const { isDate } = require("../helpers/isDate")
+const { validarCampos } = require("../middlewares/validar-campos")
 const { validarJWT } = require("../middlewares/validar-jwt")
 const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require("../controllers/events")
 
@@ -15,7 +18,16 @@ router.use( validarJWT )
 router.get('/', getEventos)
 
 // Crear un nuevo evento
-router.post('/', crearEvento)
+router.post(
+    '/',
+    [
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom( isDate ),
+        check('end', 'Fecha de finalizacion es obligatoria').custom( isDate ),
+        validarCampos
+    ],
+    crearEvento
+    )
 
 // Actualizar Evento
 router.put('/:id', actualizarEvento)
