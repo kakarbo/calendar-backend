1dd83aae0fb4b39bf79c1bbd1a297f94a5c1584e
kakarbo <alexcarbo8809@gmail.com>
2024-03-07 18:04:15 -0500

    Eliminar evento

--- controllers/events.js
+++ controllers/events.js
@@ -88,13 +88,44 @@ const actualizarEvento = async ( req, res = response ) => {
     }
 }
 
-const eliminarEvento = ( req, res = response ) => {
+const eliminarEvento = async ( req, res = response ) => {
+
+    const eventoId = req.params.id
+    const uid = req.uid
+    
+    try {
+
+        const evento = await Evento.findById( eventoId )
+
+        if ( !evento ) {
+            res.status(404).json({
+                ok: false,
+                msg: "Evento no existe por id"
+            })
+        }
+
+        if ( evento.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: "No tiene privilegio para eliminar este evento",
+            })
+        }
+
+        await Evento.findByIdAndDelete( eventoId )
+
+        res.json({
+            ok: true,
+            msg: "Evento Eliminado"
+        })
 
 
-    return res.status(200).json({
-        ok: true,
-        msg: 'Eliminar eventos',
-    })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: "Hable con el administrador"
+        })
+    }
 }
 
 module.exports = {
