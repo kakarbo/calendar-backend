2b9fb724ea8a6af714f3f3ae2e5c43ae93ee1b00
Cristhian Carbonell <ccarbone@bancolombia.com.co>
2024-03-07 17:35:55 -0500

    actualizar evento

--- controllers/events.js
+++ controllers/events.js
@@ -1,5 +1,6 @@
 const { response } = require("express")
 const Evento = require("../models/Eventos") 
+const { setRandomFallback } = require("bcryptjs")
 
 
 const getEventos = async ( req, res = response ) => {
@@ -42,12 +43,49 @@ const crearEvento = async ( req, res = response ) => {
     })
 }
 
-const actualizarEvento = ( req, res = response ) => {
+const actualizarEvento = async ( req, res = response ) => {
 
-    return res.status(200).json({
-        ok: true,
-        msg: 'Actualizar eventos',
-    })
+    const eventoId = req.params.id
+    const uid = req.uid
+
+    try {
+
+        const evento = await Evento.findById( eventoId )
+
+        if ( !evento ) {
+            res.status(404).json({
+                ok: false,
+                msg: "Evento no existe por ese id"
+            })
+        }
+
+        if ( evento.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: "No tiene privilegio de editar este evento",
+            })
+        }
+
+        const nuevoEvento = {
+            ...req.body,
+            user: uid
+        }
+
+        const eventoActualizado  = await Evento.findByIdAndUpdate( eventoId, nuevoEvento, { new: true } )
+
+        res.json({
+            ok: true,
+            evento: eventoActualizado
+        })
+
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: "Hable con el administrador"
+        })
+    }
 }
 
 const eliminarEvento = ( req, res = response ) => {
