a8570705302d2a026b60f800a5ad8f3b26e5716f
kakarbo <alexcarbo8809@gmail.com>
2024-03-13 19:46:32 -0500

    ver los logs de conección de la base de datos

--- databases/config.js
+++ databases/config.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose")
 
 const dbConnection = async() => {
 
+    console.log(`está es la url de conección a la base de datos ${process.env.DB_CNN}`)
+
     try {
 
         await mongoose.connect(process.env.DB_CNN);
